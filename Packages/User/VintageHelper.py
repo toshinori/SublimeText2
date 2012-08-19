168cf8b065c4538798280c69a1664e47967084a5
toshinori <toshinori@imaizumi.jp>
2012-08-19 20:31:53 +0900

    AutoComplete表示中でもEscキーを押したらInsertModeを抜けるようにした。

--- Packages/User/VintageHelper.py
+++ Packages/User/VintageHelper.py
@@ -3,6 +3,19 @@ import sublime, sublime_plugin
 class OnSaveComannd(sublime_plugin.EventListener):
   def on_post_save(self, view):
     if not view.settings().get('command_mode'):
+      view.run_command('hide_auto_complete')
       view.run_command('exit_insert_mode')
+
+class HideAutoCompleteAndExitInsertModeCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    print "hide"
+    if not self.view.settings().get('command_mode'):
+      self.view.run_command('hide_auto_complete')
+      self.view.run_command('exit_insert_mode')
+# class ExitInsertModeByForce(sublime_plugin.TextCommand):
+#   def run(self, view)
+#     print self
+
+
 
 
